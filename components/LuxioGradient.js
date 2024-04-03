1bb4e8316df04cfc989871b7fbd9f6fd739b1e79
Emile Nijssen <emile@emilenijssen.nl>
2024-04-03 18:12:51 +0200

    fix LuxioGradient

--- components/LuxioGradient.js
+++ components/LuxioGradient.js
@@ -1,93 +1,83 @@
 import { useState, useEffect } from 'react';
 import { View } from 'react-native';
 import {
-  Skia,
   Canvas,
-  Image,
   Rect,
   LinearGradient,
   vec,
-  useCanvasRef,
 } from '@shopify/react-native-skia';
-import {
-  withTiming,
-  useSharedValue,
-  cancelAnimation,
+import Animated, {
+  FadeIn,
 } from 'react-native-reanimated';
-
-const imageBlackData = Skia.Data.fromBase64('iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNk+A8AAQUBAScY42YAAAAASUVORK5CYII='); // 1px by 1px black image
-const imageBlack = Skia.Image.MakeImageFromEncoded(imageBlackData);
 
 export default function LuxioGradient({
   colors = ["#FF0000", "#00FF00", "#0000FF"],
   style = {},
+  duration = 300,
 }) {
   const [width, setWidth] = useState(0);
   const [height, setHeight] = useState(0);
-  const [backgroundImage, setBackgroundImage] = useState(imageBlack);
-  const [foregroundGradient, setForegroundGradient] = useState(colors);
-
-  const canvasRef = useCanvasRef();
-  const foregroundOpacity = useSharedValue(1);
+  const [gradients, setGradients] = useState([{
+    colors: ['#000000', '#000000'],
+    createdAt: Date.now(),
+  }]);
 
   useEffect(() => {
-    // cancelAnimation(foregroundOpacity);
+    // Add new colors to gradients
+    const gradientsNew = [...gradients, {
+      colors,
+      createdAt: Date.now(),
+    }];
 
-    // // Take a snapshot of the canvas, and set that as background
-    // try {
-    //   const snapshot = canvasRef.current?.makeImageSnapshot();
-    //   setBackgroundImage(snapshot);
-    // } catch (err) {
-    //   // Throws on first render
-    // }
+    // Remove oldest gradient
+    if (gradientsNew.length >= 3) {
+      gradientsNew.shift();
+    }
 
-    // Render the new colors
-    setForegroundGradient(colors);
-
-    // Animated foregroundOpacity to 100%
-    // foregroundOpacity.value = 0;
-    // foregroundOpacity.value = withTiming(1, { duration: 300 });
+    setGradients(gradientsNew);
   }, [colors]);
 
   return (
     <View
       style={style}
       collapsable={false}
+      onLayout={e => {
+        setWidth(e.nativeEvent.layout.width);
+        setHeight(e.nativeEvent.layout.height);
+      }}
     >
-      <Canvas
-        ref={canvasRef}
-        collapsable={false}
-        onLayout={e => {
-          setWidth(e.nativeEvent.layout.width);
-          setHeight(e.nativeEvent.layout.height);
-        }}
-        style={{
-          flex: 1,
-          flexGrow: 1,
-        }}
-      >
-        <Image
-          x={0}
-          y={0}
+      {gradients.map(gradient => (
+        <Animated.View
+          key={String(gradient.createdAt)}
+          entering={FadeIn.duration(duration)}
           width={width}
           height={height}
-          image={backgroundImage}
-          fit="cover"
-        />
-        <Rect
-          x={0}
-          y={0}
-          width={width}
-          height={height}
-          opacity={foregroundOpacity}
+          style={{
+            position: 'absolute',
+          }}
         >
-          <LinearGradient
-            start={vec(0, 0)}
-            end={vec(width, height)}
-            colors={foregroundGradient}
-          />
-        </Rect>
-      </Canvas>
-    </View>
+          <Canvas
+            collapsable={false}
+            style={{
+              flex: 1,
+              flexGrow: 1,
+            }}
+          >
+            <Rect
+              x={0}
+              y={0}
+              width={width}
+              height={height}
+            >
+              <LinearGradient
+                start={vec(0, 0)}
+                end={vec(width, height)}
+                colors={gradient.colors}
+              />
+            </Rect>
+          </Canvas>
+        </Animated.View>
+      ))}
+    </View >
   );
 };
